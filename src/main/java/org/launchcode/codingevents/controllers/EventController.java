648336cf00b1f9b2fc0af878266508fa51bb88cb
MaxraySavage <maxraysavage@gmail.com>
2021-11-22 15:03:01 -0500

    use jpa and mysql

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -2,10 +2,13 @@ package org.launchcode.codingevents.controllers;
 
 import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
+import org.launchcode.codingevents.models.EventType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -25,12 +28,21 @@ public class EventController {
     @GetMapping("create")
     public String renderCreateEventForm(Model model) {
         model.addAttribute("title", "Create Event");
+        model.addAttribute(new Event());
+        model.addAttribute("types", EventType.values());
         return "events/create";
     }
 
     @PostMapping("create")
-    public String createEvent(@ModelAttribute Event event) {
-        EventData.add(event);
+    public String processCreateEventForm(@ModelAttribute @Valid Event event, Errors errors, Model model) {
+
+        if(errors.hasErrors()) {
+            model.addAttribute("title", "Create Event");
+            model.addAttribute("errorMsg", "Bad");
+            return "events/create";
+        }
+        Event newEvent = new Event(event.getName(), event.getDescription(), event.getContactEmail(), event.getType());
+        EventData.add(newEvent);
         return "redirect:";
     }
 
@@ -57,13 +69,13 @@ public class EventController {
     public String displayEditForm(Model model, @PathVariable int eventId) {
         Event event = EventData.getById(eventId);
 
-        model.addAttribute("title", "Edit Event" + event + "(id=" + event.getId() + ")");
+        model.addAttribute("title", "Edit Event");
         model.addAttribute("event", event);
         return "events/edit";
     }
 
     @PostMapping("edit")
-    public String processEditForm(int eventId, String name, String description) {
+    public String processEditForm(Integer eventId, String name, String description) {
 
         Event event = EventData.getById(eventId);
         event.setName(name);
